1a5c5db2b792c6861b99d72e0cd188875c33edd8
proethiopia <mintesnotbissare@gmail.com>
2023-05-09 16:49:36 -0400

    master

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+  public static void main(String[] args) throws Exception {
+    for (int x = 1; x < 100; x++) {
+      if (x % 3 == 0) {
+        System.out.println("fizz");
+      } else if (x % 5 == 0) {
+        System.out.println("buzz");
+      } else if (x % 5 == 0 && x % 3 == 0) {
+        System.out.println("buzzfizz");
+      } else {
+        System.out.println(x + "this number");
+      }
+    }
+  }
+}
