d301040b486e983bb27a2501877dd99615d309a2
proethiopia <132314936+proethiopia@users.noreply.github.com>
2023-05-09 20:31:58 -0400

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,12 +1,12 @@
 public class fizzBuzz {
   public static void main(String[] args) throws Exception {
     for (int x = 1; x < 100; x++) {
-      if (x % 3 == 0) {
-        System.out.println("fizz");
+      if  (x % 5 == 0 && x % 3 == 0) {
+         System.out.println("buzzfizz");
       } else if (x % 5 == 0) {
         System.out.println("buzz");
-      } else if (x % 5 == 0 && x % 3 == 0) {
-        System.out.println("buzzfizz");
+      } else if (x % 3 == 0) {
+       System.out.println("fizz");
       } else {
         System.out.println(x + "this number");
       }
